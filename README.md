be9f4afabea97329d66092f628a3e82080ac77ad
VTC <vtchris@hotmail.com>
2019-06-28 09:38:04 -0400

    Remove played words from array, start next round 4 seconds after current round ends, include gif in readme, and update readme.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# node-word-guess
-Node Word Guess
+#Node Word Guess
+
+#TECHNOLOGIES
+<ul>
+    <li>JavaScript</li>
+    <li>Node.js</li>    
+</ul>
+
+#CONCEPTS APPLIED
+<ul>
+    <li>Object Constructors</li>
+    <li>Modular Code</li>
+    <li>Inquirer for user interaction</li>    
+</ul>
+
+
+![](assets/images/demo.gif)
+<ul>
+    <li>From command line, in appropriate folder, type `node index`</li>
+    <li>A random word will be selected and you will be prompted to guess a letter</li>
+    <li>Type a letter and press `enter` </li>
+    <li>If the letter is a part of the word it will be displayed, otherwise your remaining guesses will be reduced. </li>
+    <li>Round Ends when you correctly guess all the letters, or run out of remaining guesses.</li>
+    <li>Thanks for visiting, enjoy the game!</li>
+</ul>
--- index.js
+++ index.js
@@ -11,11 +11,14 @@ var word = "";
 newGame()
 
 function newGame() {
-      
+    
+    //Get random word from array
     let random = Math.floor(Math.random() * wordsArr.length)
     word = new Word()
     word.setWord(wordsArr[random])
     display_game();
+    //Remove current word from array
+    wordsArr.splice(random,1)    
     request_letter();
 
 }
@@ -33,6 +36,7 @@ function request_letter() {
         let userGuess = response.userGuess.toLowerCase();
         let alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+        //Validate the user guess
         if(userGuess.length > 1 || alphabet.indexOf(userGuess) < 0 ){
 
             console.log("Please enter one alpha character only.")
@@ -49,16 +53,21 @@ function request_letter() {
             word.guessLetter(userGuess);
             let displayWord = word.toString();
 
+            //If current guess is not part of the word
             if (displayWord.indexOf(userGuess) < 0) {
+
                 word.remainingGuesses--
 
                 if(word.remainingGuesses <=0){
                     losses++
                     display_game();
+                    console.log("YOU LOSE!");
+                    setTimeout(newGame,5000)
+                    return;
                 }
 
             }
-
+            //If the word contains _ then ask for the next letter
             if (displayWord.indexOf("_") > -1) {
                 display_game();
                 request_letter();
@@ -66,8 +75,10 @@ function request_letter() {
                 wins++;
                 display_game();
                 console.log("YOU WIN!");
+                setTimeout(newGame,5000)
             }
 
+        //Letter already guessed, ask again
         } else {
             display_game();
             request_letter();
@@ -77,14 +88,6 @@ function request_letter() {
 }
 function display_game() {
 
-    
-   
-
-    console.log(`\nWins: ${wins}\nLosses: ${losses}`.yellow + `\n\n${word.toString(word.remainingGuesses).toUpperCase()}\n\nRemaining Guesses: ${word.remainingGuesses}\nGuessed: ${word.guessedLetters.sort().join(" ")}
-
-    `);
-
-}
-function end_game(){
+    console.log(`\nGUESS THE MARVEL CHARACTER!\nWins: ${wins}\nLosses: ${losses}`.yellow + `\n\n${word.toString(word.remainingGuesses).toUpperCase()}\n\nRemaining Guesses: ${word.remainingGuesses}\nGuessed: ${word.guessedLetters.sort().join(" ")}`);
 
 }
